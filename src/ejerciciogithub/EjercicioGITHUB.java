ecbfea0aace7760fde382f0fda4c7c963b5f36d6
migue <migue@DESKTOP-7HK5J9T>
2021-04-21 12:18:59 -0600

    hola

--- src/ejerciciogithub/EjercicioGITHUB.java
+++ src/ejerciciogithub/EjercicioGITHUB.java
@@ -1,9 +1,18 @@
 //EJERCICIO PARA MOSTRAR EN GITHUB
 package ejerciciogithub;
+
+import java.util.Scanner;
+
 public class EjercicioGITHUB {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        String nombre;
+        Scanner teclado= new Scanner (System.in);
+        System.out.println("Introduce tu nombre por favor");
+        nombre=teclado.nextLine();
+        
+        System.out.println("Hola mundo " + nombre + " tu nombre comienza por " + nombre.substring(0, 1));
+        
     }
     
 }
